2a2c29354a589194afc14b705fc642dfbe6d5175
gurumohanreddy <gurumohan449@gmail.com>
2015-12-22 18:49:00 +0530

    Introduced fileupload

--- src/app/components/managestudent/managestudent.controller.js
+++ src/app/components/managestudent/managestudent.controller.js
@@ -6,11 +6,33 @@ myApp.controller('managestudentcontroller',['$localStorage',function($localStora
         }
 
         managestudent.addstudent = function(){
+
+          var filesSelected = document.getElementById("fileinput").files;
+          if (filesSelected.length > 0) {
+            var fileToLoad = filesSelected[0];
+            var fileReader = new FileReader();
+            var imageString;
+
+            fileReader.onload = function(fileLoadedEvent) {
+            imageString = fileLoadedEvent.target.result; // <--- data: base64
+            managestudent.addReceipt(imageString);
+            }
+          fileReader.readAsDataURL(fileToLoad);
+
+          }
+          managestudent.addReceipt = function(imageString){
+            managestudent.newstudent.studentpic = imageString;
             managestudent.storage.push(managestudent.newstudent);
              managestudent.initializenewstudent();
              $('#myModal').modal('toggle');
              console.log(managestudent.newstudent);
+
+          }
+
+
         }
+
+
 
 
         managestudent.initializenewstudent();
@@ -18,5 +40,21 @@ myApp.controller('managestudentcontroller',['$localStorage',function($localStora
         $localStorage.storage = $localStorage.storage || [];
         managestudent.storage = $localStorage.storage;
 
-
 }]);
+
+
+// myApp.directive("fileInput",['$parse',function($parse){
+//
+//     return{
+//       restrict: 'A',
+//       link: function(scope,ele,attrs){
+//           ele.bind('change',function(){
+//             $parse(attrs.fileInput)
+//             .assign(scope,ele[0].files)
+//             scope.$apply()
+//             console.log("tingu");
+//           })
+//       }
+//     }
+//
+// }]);
